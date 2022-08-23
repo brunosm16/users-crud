e8f261c29e6a6f46bf68df0c5b2eb9996b6e38b7
brunosm16 <brunoskr23@gmail.com>
2022-08-23 10:18:40 -0300

    feat: add jsonPlaceHolderAPI and localAPI.

--- src/http-utils/fetch-api.js
+++ src/http-utils/fetch-api.js
@@ -1,18 +1,20 @@
 /* eslint-disable implicit-arrow-linebreak */
-import httpHelper from './http-helper';
+import { baseFetch, jsonPlaceHolderAPI } from './http-helper';
+
+export const getTodos = async (userId) => jsonPlaceHolderAPI.get('/todos', { params: { userId } });
 
 export const getHttp = async (url, params, options) =>
-  httpHelper(url, 'get', { ...options, params });
+  baseFetch(url, 'get', { ...options, params });
 
-export const postHttp = async (url, data, options) => httpHelper(url, 'post', { data, ...options });
+export const postHttp = async (url, data, options) => baseFetch(url, 'post', { data, ...options });
 
-export const putHttp = async (url, data, options) => httpHelper(url, 'put', { data, ...options });
+export const putHttp = async (url, data, options) => baseFetch(url, 'put', { data, ...options });
 
 export const patchHttp = async (url, data, options) =>
-  httpHelper(url, 'patch', { data, ...options });
+  baseFetch(url, 'patch', { data, ...options });
 
 export const updateHttp = async (url, data, options) =>
-  httpHelper(url, 'update', { data, ...options });
+  baseFetch(url, 'update', { data, ...options });
 
 export const deleteHttp = async (url, data, options) =>
-  httpHelper(url, 'delete', { data, ...options });
+  baseFetch(url, 'delete', { data, ...options });
--- src/http-utils/http-helper.js
+++ src/http-utils/http-helper.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-export default async (url, method, options = {}) => axios({
+const createAxios = (options = {}) => axios.create({ ...options });
+
+const localAPI = createAxios();
+
+export const jsonPlaceHolderAPI = createAxios({ baseURL: 'https://jsonplaceholder.typicode.com/' });
+
+export const baseFetch = async (url, method, options = {}) => localAPI({
   method: method.toUpperCase(),
   url,
   ...options,
