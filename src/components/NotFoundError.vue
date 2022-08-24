976e20cbd00b0a04cecc9cd1fbff142f0af5a0f3
brunosm16 <brunoskr23@gmail.com>
2022-08-24 10:33:04 -0300

    feat: add component to handle error.

---
+++ src/components/NotFoundError.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="error-404">
+    <span>NotFoundError Component</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundError',
+};
+</script>
+
+<style lang="scss" scoped></style>
