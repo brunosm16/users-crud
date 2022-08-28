28c1424605810f7377881dd77c22f032367ff34a
brunosm16 <brunoskr23@gmail.com>
2022-08-28 20:17:13 -0300

    refactor interceptors to new payload.

--- src/http-utils/interceptors.js
+++ src/http-utils/interceptors.js
@@ -7,53 +7,55 @@ const methodsBodyIdForbidden = ['post', 'remove'];
 
 const hasIdField = (data) => Object.prototype.hasOwnProperty.call(data, 'id');
 
-const validateUserData = (data) => {
-	if (!data && !data.userData) {
-		throw new Error('No userData was provided');
-	}
+const validateNoDataProvided = (data) => {
+  if (!data) {
+    throw new Error('No UserData was provided');
+  }
 };
 
 const validateRequiredId = ({ data }) => {
-	validateUserData(data);
+  const userData = data?.data;
 
-	const { userData } = data;
+  validateNoDataProvided(userData);
 
-	if (!hasIdField(userData[0]) || !userData[0].id) {
-		throw new Error('ID is required for this request');
-	}
+  if (!hasIdField(userData) || !userData.id) {
+    throw new Error('ID is required for this request');
+  }
 };
 
 const validateForbiddenId = ({ data }) => {
-	validateUserData(data);
+  const userData = data?.data;
 
-	const { userData } = data;
+  validateNoDataProvided(userData);
 
-	if (hasIdField(userData[0])) {
-		throw new Error('ID is forbidden for this request');
-	}
+  if (hasIdField(userData)) {
+    throw new Error('ID is forbidden for this request');
+  }
 };
 
 export const requestInterceptor = async (config) => {
-	const hasRequiredBodyIdMethod = methodsBodyIdRequired.includes(config.method.toLocaleLowerCase());
-	const hasForbiddenBodyIdMethod = methodsBodyIdForbidden.includes(
-		config.method.toLocaleLowerCase(),
-	);
+  const hasRequiredBodyIdMethod = methodsBodyIdRequired.includes(
+    config.method.toLocaleLowerCase(),
+  );
+  const hasForbiddenBodyIdMethod = methodsBodyIdForbidden.includes(
+    config.method.toLocaleLowerCase(),
+  );
 
-	if (hasRequiredBodyIdMethod) {
-		validateRequiredId(config);
-	}
+  if (hasRequiredBodyIdMethod) {
+    validateRequiredId(config);
+  }
 
-	if (hasForbiddenBodyIdMethod) {
-		validateForbiddenId(config);
-	}
+  if (hasForbiddenBodyIdMethod) {
+    validateForbiddenId(config);
+  }
 
-	return config;
+  return config;
 };
 
 export const responseInterceptor = (response) => response;
 
 export const errorInterceptor = (error) => {
-	Swal.fire('Error!', `${error.message}`, 'error');
+  Swal.fire('Error!', `${error.message}`, 'error');
 
-	return Promise.reject(error);
+  return Promise.reject(error);
 };
