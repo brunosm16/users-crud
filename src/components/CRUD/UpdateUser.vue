8f026aca886353f0b1823b7bb436750c04efc8c9
brunosm16 <brunoskr23@gmail.com>
2022-08-29 09:54:19 -0300

    fetch user on update-user.

--- src/components/CRUD/UpdateUser.vue
+++ src/components/CRUD/UpdateUser.vue
@@ -5,13 +5,37 @@
 </template>
 
 <script>
+import { getHttp } from '@/http-utils/fetch-api';
+import ChangeComponent from '@/mixins/change-component';
+import ApiUrl from '@/mixins/api-url';
+
 export default {
   name: 'UpdateUser',
 
   inject: ['userId'],
 
-  created() {
-    console.log(`Update user with id : ${this.userId}`);
+  mixins: [ChangeComponent, ApiUrl],
+
+  data: () => ({
+    user: {
+      name: '',
+      email: '',
+      age: '',
+      company: '',
+      phone: '',
+      address: '',
+    },
+  }),
+
+  async beforeMount() {
+    await this.fetchUserById(this.userId);
+  },
+
+  methods: {
+    async fetchUserById(id) {
+      const { data } = await getHttp(`${this.apiURL}/${id}`);
+      this.user = data;
+    },
   },
 };
 </script>
