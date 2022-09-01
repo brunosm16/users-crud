9464cc8bc7c34dea05d6c4603a987b571fb00c5d
brunosm16 <brunoskr23@gmail.com>
2022-09-01 09:42:41 -0300

    fix list-users styles.

--- src/views/User/ListUsers.vue
+++ src/views/User/ListUsers.vue
@@ -1,110 +1,116 @@
 <!-- eslint-disable vue/no-deprecated-v-on-native-modifier -->
 <template>
   <div class="list-users-component">
-    <vs-card class="list-users-component__card">
-      <template #header>
-        <h3 class="list-users-component__card--header">Users List</h3>
-      </template>
+    <vs-row>
+      <vs-col vs-type="flex" vs-justify="left" vs-align="left" vs-w="12">
+        <vs-card class="list-users-component__card">
+          <template #header>
+            <h3 class="list-users-component__card--header">Users List</h3>
+          </template>
 
-      <vs-row>
-        <vs-col vs-type="flex" vs-justify="left" vs-align="left" vs-w="12">
-          <vs-table :data="users" search strip pagination max-items="5">
-            <template #thead>
-              <vs-th sort-key="userId"> # </vs-th>
+          <vs-row>
+            <vs-col vs-type="flex" vs-justify="left" vs-align="left" vs-w="12">
+              <vs-table :data="users" search strip pagination max-items="5">
+                <template #thead>
+                  <vs-th sort-key="userId"> # </vs-th>
 
-              <vs-th> Name </vs-th>
+                  <vs-th> Name </vs-th>
 
-              <vs-th> Email </vs-th>
+                  <vs-th> Email </vs-th>
 
-              <vs-th> Age </vs-th>
+                  <vs-th> Age </vs-th>
 
-              <vs-th> Company </vs-th>
+                  <vs-th> Company </vs-th>
 
-              <vs-th> Phone </vs-th>
+                  <vs-th> Phone </vs-th>
 
-              <vs-th> Address </vs-th>
+                  <vs-th> Address </vs-th>
 
-              <vs-th> Actions </vs-th>
-            </template>
+                  <vs-th> Actions </vs-th>
+                </template>
 
-            <template v-slot:default="dataTable">
-              <vs-tr v-for="(tr, index) in dataTable.data" :key="index">
-                <vs-td :data="dataTable?.data[index]?.id">
-                  {{ dataTable?.data[index]?.id }}
-                </vs-td>
+                <template v-slot:default="dataTable">
+                  <vs-tr v-for="(tr, index) in dataTable.data" :key="index">
+                    <vs-td :data="dataTable?.data[index]?.id">
+                      {{ dataTable?.data[index]?.id }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.name">
-                  {{ dataTable?.data[index]?.name }}
-                </vs-td>
+                    <vs-td :data="dataTable?.data[index]?.name">
+                      {{ dataTable?.data[index]?.name }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.email">
-                  {{ dataTable?.data[index]?.email }}
-                </vs-td>
+                    <vs-td :data="dataTable?.data[index]?.email">
+                      {{ dataTable?.data[index]?.email }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.age">
-                  {{ dataTable?.data[index]?.age }}
-                </vs-td>
+                    <vs-td :data="dataTable?.data[index]?.age">
+                      {{ dataTable?.data[index]?.age }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.company">
-                  {{ dataTable?.data[index]?.company }}
-                </vs-td>
+                    <vs-td :data="dataTable?.data[index]?.company">
+                      {{ dataTable?.data[index]?.company }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.phone">
-                  {{ dataTable?.data[index]?.phone }}
-                </vs-td>
+                    <vs-td :data="dataTable?.data[index]?.phone">
+                      {{ dataTable?.data[index]?.phone }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.address">
-                  {{ dataTable?.data[index]?.address }}
-                </vs-td>
+                    <vs-td :data="dataTable?.data[index]?.address">
+                      {{ dataTable?.data[index]?.address }}
+                    </vs-td>
 
-                <vs-td :data="dataTable?.data[index]?.id">
-                  <vs-button
-                    color="primary"
-                    type="filled"
-                    icon="remove_red_eye"
-                    size="small"
-                    @click.native="
-                      changeRoute('view-user', dataTable?.data[index]?.id)
-                    "
-                  />
-                  <vs-button
-                    color="success"
-                    type="filled"
-                    icon="edit"
-                    size="small"
-                    @click.native="
-                      changeRoute('update-user', dataTable?.data[index]?.id)
-                    "
-                  />
+                    <vs-td :data="dataTable?.data[index]?.id">
+                      <vs-button
+                        color="primary"
+                        type="filled"
+                        icon="remove_red_eye"
+                        size="small"
+                        @click.native="
+                          changeRoute('view-user', dataTable?.data[index]?.id)
+                        "
+                      />
+                      <vs-button
+                        color="success"
+                        type="filled"
+                        icon="edit"
+                        size="small"
+                        @click.native="
+                          changeRoute('update-user', dataTable?.data[index]?.id)
+                        "
+                      />
 
-                  <vs-button
-                    color="danger"
-                    type="filled"
-                    icon="delete"
-                    size="small"
-                    @click.native="deleteUserById(dataTable?.data[index]?.id)"
-                  />
-                </vs-td>
-              </vs-tr>
-            </template>
-          </vs-table>
-        </vs-col>
-      </vs-row>
+                      <vs-button
+                        color="danger"
+                        type="filled"
+                        icon="delete"
+                        size="small"
+                        @click.native="
+                          deleteUserById(dataTable?.data[index]?.id)
+                        "
+                      />
+                    </vs-td>
+                  </vs-tr>
+                </template>
+              </vs-table>
+            </vs-col>
+          </vs-row>
 
-      <template #footer>
-        <vs-row vs-justify="flex-start">
-          <vs-button
-            color="primary"
-            type="filled"
-            icon="fiber_new"
-            size="small"
-            @click.native="changeRoute('create-user')"
-          >
-            Create A New User
-          </vs-button>
-        </vs-row>
-      </template>
-    </vs-card>
+          <template #footer>
+            <vs-row vs-justify="flex-start">
+              <vs-button
+                color="primary"
+                type="filled"
+                icon="fiber_new"
+                size="small"
+                @click.native="changeRoute('create-user')"
+              >
+                Create A New User
+              </vs-button>
+            </vs-row>
+          </template>
+        </vs-card>
+      </vs-col>
+    </vs-row>
   </div>
 </template>
 
