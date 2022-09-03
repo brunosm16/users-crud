b3b65734e01f61ec36da19a6d18091829c1be79d
brunosm16 <brunoskr23@gmail.com>
2022-09-02 21:45:53 -0300

    fix imports and setup store.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import './server/server';
-import router from '@/router/index';
 import Vue from 'vue';
 import Vuesax from 'vuesax';
+import Vuex from 'vuex';
+
+import router from '@/router';
+import store from '@/store';
 import App from './App.vue';
 
+import './server/server';
+
 Vue.use(Vuesax);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
