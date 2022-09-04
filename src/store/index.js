1252b316e36dafa15555efacbf9a4fb13d5ab567
brunosm16 <brunoskr23@gmail.com>
2022-09-04 17:26:24 -0300

    set hot-reload for vuex.

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,36 @@
+/* eslint-disable import/no-import-module-exports */
+/* eslint-disable global-require */
 import Vue from 'vue';
 import Vuex from 'vuex';
 import UsersStore from './user';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   ...UsersStore,
 });
+
+if (module.hot) {
+  const hmr = [
+    './user',
+    './user/getters',
+    './user/actions',
+    './user/mutations',
+  ];
+
+  const reloadCallback = () => {
+    const getters = require('./user/getters').default;
+    const actions = require('./user/actions').default;
+    const mutations = require('./user/mutations').default;
+
+    store.hotUpdate({
+      getters,
+      actions,
+      mutations,
+    });
+  };
+
+  module.hot.accept(hmr, reloadCallback);
+}
+
+export default store;
