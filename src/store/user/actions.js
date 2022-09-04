bf7cd1752785e55ef75b942a3f130f7090d152bd
brunosm16 <brunoskr23@gmail.com>
2022-09-04 15:42:52 -0300

    add actions for users.

---
+++ src/store/user/actions.js
@@ -0,0 +1,86 @@
+import {
+  getHttp,
+  postHttp,
+  patchHttp,
+  deleteHttp,
+} from '@/http-utils/fetch-api';
+
+import MutationTypes from './types';
+
+const API_ENDPOINT = '/api/users';
+
+const setLoadingTrue = (commit) => commit(MutationTypes.SET_LOADING, true);
+
+const setErrorTrue = (commit) => commit(MutationTypes.SET_ERROR, true);
+
+const fetchUsersList = async ({ commit }) => {
+  try {
+    setLoadingTrue(commit);
+
+    const { userData } = await getHttp(API_ENDPOINT);
+
+    commit(MutationTypes.SET_USERS_LIST, userData);
+  } catch {
+    setErrorTrue(commit);
+  }
+};
+
+const fetchUserById = async ({ commit }, userId) => {
+  try {
+    setLoadingTrue(commit);
+
+    const { userData } = await getHttp(`${API_ENDPOINT}/${userId}`);
+
+    commit(MutationTypes.SET_USER_DATA, userData);
+  } catch {
+    setErrorTrue(commit);
+  }
+};
+
+const createUser = async ({ commit }, payload) => {
+  try {
+    setLoadingTrue(commit);
+
+    const { userData } = await postHttp(API_ENDPOINT, {
+      data: { ...payload },
+    });
+
+    commit(MutationTypes.SET_USER_DATA, userData);
+  } catch {
+    setErrorTrue(commit);
+  }
+};
+
+const updateUser = async ({ commit }, payload) => {
+  try {
+    setLoadingTrue(commit);
+
+    const { userData } = await patchHttp(`${API_ENDPOINT}/${payload.id}`, {
+      data: { ...payload },
+    });
+
+    commit(MutationTypes.UPDATE_USER, userData);
+  } catch {
+    setErrorTrue(commit);
+  }
+};
+
+const removeUser = async ({ commit }, userId) => {
+  try {
+    setLoadingTrue(commit);
+
+    await deleteHttp(`${API_ENDPOINT}/${userId}`);
+
+    commit(MutationTypes.REMOVE_USER, userId);
+  } catch {
+    setErrorTrue(commit);
+  }
+};
+
+export default {
+  fetchUsersList,
+  fetchUserById,
+  createUser,
+  updateUser,
+  removeUser,
+};
